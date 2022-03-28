47cda698eea76baa2721759022d548a08dfc9768
Samir Maharjan <mail.samirmaharjan@gmail.com>
2022-03-28 21:17:10 +0545

    DB Con Added

---
+++ WebAPI-PriceKeeper/Migrations/20220328152731_Samir.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI_PriceKeeper.Migrations
+{
+    public partial class Samir : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
